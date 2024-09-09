09aee9675d1e3b1e67c5ea7b8b0732331ff0c450
ParthaI <partha@turbot.com>
2024-09-09 18:51:58 +0530

    Update

--- utils/format.go
+++ utils/format.go
@@ -8,7 +8,7 @@ import (
 // Define the layout for marshaling/unmarshaling. Modify this based on your API's date-time format.
 // const DateTimeLayout1 = "2006-01-02T15:04:05Z" // 2023-05-10T19:16:33.208Z
 const DateTimeLayout2 = "2006-01-02"
-const DateTimeLayout3 = "2006-01-02T15:04:05.000Z"
+// const DateTimeLayout3 = "2006-01-02T15:04:05.000Z"
 
 // 2023-05-10T19:16:33.208Z
 
@@ -17,62 +17,37 @@ func MarshalDateTime(t *time.Time) ([]byte, error) {
 	if t == nil {
 		return nil, fmt.Errorf("time value is nil")
 	}
-	return []byte(t.Format(DateTimeLayout3)), nil
+	return []byte(t.Format(DateTimeLayout2)), nil
 }
 
 // UnmarshalDateTime converts a string from the GraphQL response to a time.Time.
 func UnmarshalDateTime(src []byte, dst *time.Time) error {
-	// Trim quotes if the string is quoted
-	// src = bytes.Trim(src, "\\\"")
 
-	// Convert byte slice to string
-	// srcStr := strings.TrimSpace(string(src))
-	// srcStr = strings.Trim(srcStr, "\"")
-	// if srcStr == "" {
-	// 	return nil
-	// }
-
-	// Define a list of possible layouts to try
-	// layouts := []string{
-	// 	DateTimeLayout3,
-	// }
 
 	// Try each layout in sequence
 	// time.Parse(`"`+dateFormat+`"`, string(b))
-	var parsedTime2, parsedTime3 time.Time
-	var parseErr2, parseErr3 error
-	parsedTime2, parseErr2 = time.Parse(`"`+DateTimeLayout2+`"`, string(src))
+	// var parsedTime2, parsedTime3 time.Time
+	// var parseErr2, parseErr3 error
+	parsedTime2, parseErr2 := time.Parse(`"`+DateTimeLayout2+`"`, string(src))
 	if parseErr2 != nil {
-		parsedTime3, parseErr3 = time.Parse(`"`+DateTimeLayout3+`"`, string(src))
+		return parseErr2
 	}
 
-	if parseErr3 != nil && parseErr2 != nil {
-		if parseErr3 != nil {
-			return parseErr3
-		}
-		if parseErr2 != nil {
-			return parseErr2
-		}
-	}
-	if parseErr3 == nil {
-		*dst = parsedTime3
-	}
-	if parseErr2 == nil {
-		*dst = parsedTime2
-	}
-	// var err []error
-	// for _, layout := range layouts {
-	// 	parsedTime, parseErr := time.Parse(layout, srcStr)
-	// 	if parseErr == nil {
-	// 		*dst = parsedTime
-	// 		return nil
+	*dst = parsedTime2
+
+	// if parseErr3 != nil && parseErr2 != nil {
+	// 	if parseErr3 != nil {
+	// 		return parseErr3
 	// 	}
-	// 	err = append(err, parseErr)
+	// 	if parseErr2 != nil {
+	// 		return parseErr2
+	// 	}
 	// }
-
-	// // If none of the layouts succeeded, return the last error
-	// if len(err) > 2 {
-	// 	return err[0]
+	// if parseErr3 == nil {
+	// 	*dst = parsedTime3
+	// }
+	// if parseErr2 == nil {
+	// 	*dst = parsedTime2
 	// }
 
 	return nil
